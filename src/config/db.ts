85f612b32b331a3a468b193d39be4292e73f17c3
Camila Serantoni <camila.serantoni@blue.cl>
2024-05-30 07:49:03 -0400

    se agrega libreria colors y se mejoran mensajes para  mejor visualizacion

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,8 +1,7 @@
 import { Sequelize } from "sequelize";
 import dotenv from 'dotenv';
-
 dotenv.config();
-console.log();
+
 
 const db = new Sequelize(process.env.DATABASE_URL!)
 
--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,21 @@
 import express from 'express';  
 import productsRouter from './router';
 import db from './config/db';
-
+import colors from 'colors';
 
 //Conectar a la base de datos
 async function connectDB() {
     try {
 await db.authenticate();
 db.sync();
-console.log('Conexion exitosa a la BD');
+console.log(colors.blue('Conexion exitosa a la BD'));
     }catch (error) {
-console.log(error)
-console.log('No se pudo conectar a la BD');
+// console.log(error)
+console.log(colors.red.bold(  'No se pudo conectar a la BD'));
 }
 }
+
+connectDB();
 
 
 const server = express();
