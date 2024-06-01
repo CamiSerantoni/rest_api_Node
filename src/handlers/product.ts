e03dc3acf07d5ed72c081582e785679563b59991
Camila Serantoni <camila.serantoni@blue.cl>
2024-06-01 09:39:15 -0400

    se agrega obtener producto  por id

--- src/handlers/product.ts
+++ src/handlers/product.ts
@@ -3,28 +3,56 @@ import Product from '../models/Product.model';
 
 
 
-export const getProducts = async (req : Request, res : Response) => {
-try{
-   //Para obtener datos es bueno usar finds
-   const products =  await Product.findAll({
+export const getProducts = async (req: Request, res: Response) => {
+   try {
+      //Para obtener datos es bueno usar finds
+      const products = await Product.findAll({
+         //si quieres darle orden a la respuesta 
+         order: [['id', 'DESC']],
+         //Si quieres que no se muestren ciertos elementos /campos   
+         //attributes:{exclude: ['createdAt', 'updatedAt', 'availability']},
+         //si quieres limitar la respuesta
+         // limit: 2
+      });
+      res.json({ data: products });
+   } catch (err) {
+      console.log(err)
+   }
+}
+
+export const getProductById = async (req: Request, res: Response) => {
+   try {
+      //Para obtener datos es bueno usar finds
+    
+      const { id } = req.params
+      const product = await Product.findByPk(id);
+
+      if (!product) {
+         return res.status(404).json({ error: 'Producto no encontrado' })
+      }
+
+       res.json({ data: product });
       //si quieres darle orden a la respuesta 
-      order: [['id', 'DESC']],
+
       //Si quieres que no se muestren ciertos elementos /campos   
       //attributes:{exclude: ['createdAt', 'updatedAt', 'availability']},
       //si quieres limitar la respuesta
       // limit: 2
-   });
-   res.json({data: products});
-}catch(err){
-   console.log(err)
+
+   } catch (err) {
+      console.log(err)
+   }
+
 }
-}
-export const createProduct = async (req : Request, res : Response) => {
- try{
-    const product =  await Product.create(req.body);
-    res.json({data: product});
- }catch(err){
-    console.log(err)
- }
+
+
+
+export const createProduct = async (req: Request, res: Response) => {
+   try {
+      const product = await Product.create(req.body);
+      res.json({ data: product });
+   } catch (err) {
+      console.log(err)
+   }
 }
 
--- src/router.ts
+++ src/router.ts
@@ -1,31 +1,38 @@
 import { Router } from "express";
-import { createProduct, getProducts } from './handlers/product';
-import { body } from "express-validator";
+import { createProduct, getProductById, getProducts } from './handlers/product';
+import { body, param } from "express-validator";
 import { handleInputErrors } from "./middleware";
 
 const router = Router();
 
 
 //Routing
-router.get('/',  getProducts)
+router.get('/', getProducts)
+router.get('/:id',
+
+    param('id').isInt().withMessage('El id del producto debe ser numerico'),
+    handleInputErrors,
+    getProductById
 
 
+)
 
-router.post('/',  
+
+router.post('/',
 
 
 
     //Validaciones
-   body('name').notEmpty().withMessage('El nombre del producto no puede ser vacío'),
-                       
+    body('name').notEmpty().withMessage('El nombre del producto no puede ser vacío'),
+
     body('price')
-                .isNumeric().withMessage('El precio del Producto debe ser numerico')
-                .not().isEmpty().withMessage('El precio del Producto no puede ser vacío') 
-                .notEmpty().withMessage('El precio del producto no puede ser vacío')
-                .custom(value => value > 0).withMessage('El precio debe ser mayor a 0'),
-              handleInputErrors,
-              createProduct
-) 
+        .isNumeric().withMessage('El precio del Producto debe ser numerico')
+        .not().isEmpty().withMessage('El precio del Producto no puede ser vacío')
+        .notEmpty().withMessage('El precio del producto no puede ser vacío')
+        .custom(value => value > 0).withMessage('El precio debe ser mayor a 0'),
+    handleInputErrors,
+    createProduct
+)
 
 
 router.put('/', (req, res) => {
